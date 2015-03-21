78a8df51b5610bd074958ad331c1000e39659329
Elizabeth Hale <lizzhale@mac.com>
2015-03-21 15:32:24 -0700

    JSON exercise

--- server.js
+++ server.js
@@ -4,10 +4,30 @@ var app = express();
 
 app.set("view engine", "ejs");
 
-app.get('/json', function (req, res) {
-  request('http//daretodiscover.herokuapp.com/users', function (error, response, body) {
-    res.render("home", {
-      home: body
+app.get('/users', function (req, res) {
+  request('http://daretodiscover.herokuapp.com/users', function (error, response, body) {
+    res.render("index", {
+      users: JSON.parse(body)
+    });
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      console.log(data);
+    }
+  });
+});
+
+app.get('/last', function (req, res) {
+  request('http://daretodiscover.herokuapp.com/users', function (error, response, body) {
+    res.render("last", {
+      users: JSON.parse(body)
+    });
+  });
+});
+
+app.get('/first', function (req, res) {
+  request('http://daretodiscover.herokuapp.com/users', function (error, response, body) {
+    res.render("first", {
+      users: JSON.parse(body)
     });
   });
 });
